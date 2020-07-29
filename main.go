0da05d3daf353febbf5f5faa6db2a55207759821
Dmitry Sky <dmitry.sky@icloud.com>
2020-07-28 14:04:53 -1000

    Define Block structure

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
-import (
-  "fmt"
-  "rsc.io/quote"
-)
+type Block struct {
+  Hash      []byte
+  Data      []byte
+  PrevHash  []byte
+}
 
 func main()  {
-  fmt.Println(quote.Hello())
+
 }
