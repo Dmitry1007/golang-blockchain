99b5ca8fe86e96141e73dc935afdeefdb1d8d068
Dmitry Sky <dmitry.sky@icloud.com>
2020-07-31 10:50:16 -1000

    Psuedo Code

---
+++ blockchain/proof.go
@@ -0,0 +1,13 @@
+package blockchain
+
+
+// Take the data from the block
+
+// Create a counter (nonce) which starts at 0
+
+// Create a hash of the data plus the counter
+
+// Check the hash to see if it meets a set of requirements
+
+// Requirements:
+// The First few bytes of the hash must contain 0s
