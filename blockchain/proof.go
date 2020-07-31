6818d0759205bdc312a68d8f8e6d84fe6526581a
Dmitry Sky <dmitry.sky@icloud.com>
2020-07-31 11:07:36 -1000

    Create a proof for a specific block

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -1,5 +1,8 @@
 package blockchain
 
+import (
+  "fmt"
+)
 
 // Take the data from the block
 
@@ -11,3 +14,17 @@ package blockchain
 
 // Requirements:
 // The First few bytes of the hash must contain 0s
+
+const Difficulty = 12
+
+type ProofOfWork struct {
+  Block  *Block
+  Target *big.Int
+}
+
+func NewProof(b *Block) *ProofOfWork {
+  target := big.NewInt(1)
+  target.Lsh(target, uint(256-Difficulty))
+  pow := &ProofOfWork{b, target}
+  return pow
+}
