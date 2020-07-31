e3fda7e329a768729219934830c0e51b9ba3efd9
Dmitry Sky <dmitry.sky@icloud.com>
2020-07-31 11:44:38 -1000

    Define entire proof of work algorithm

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -42,6 +42,27 @@ func (pow *ProofOfWork) InitData(nonce int) []byte {
   return data
 }
 
+func (pow *ProofOfWork) Run() (int, []byte) {
+  var intHash big.Int
+  var hash [32]byte
+  nonce := 0
+  for nonce < math.MaxInt64 {
+    data := pow.InitData(nonce)
+    hash := sha256.Sum256(data)
+
+    fmt.Printf("\r%x", hash)
+    intHash.SetBytes(hash[:])
+
+    if initHash.Cmp(pow.Target) == -1 {
+      break
+    } else {
+      nonce++
+    }
+  }
+  fmt.Println()
+  return nonce, hash[:]
+}
+
 func ToHex(num int64) []byte {
   buff := new(bytes.Buffer)
   err  := binary.Write(buff, binary.BigEndian, num)
