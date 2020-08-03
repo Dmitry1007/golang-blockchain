238260e1ae509837517a1c39c843814755a9b4ea
Dmitry Sky <dmitry.sky@icloud.com>
2020-08-03 13:35:24 -1000

    Can't believe I missed that :

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -30,7 +30,7 @@ type ProofOfWork struct {
 
 func NewProof(b *Block) *ProofOfWork {
   target := big.NewInt(1)
-  target.Lsh(target, uint(256-Difficulty))
+  target.Lsh(target, uint(256 - Difficulty))
   pow := &ProofOfWork{b, target}
   return pow
 }
@@ -54,9 +54,9 @@ func (pow *ProofOfWork) Run() (int, []byte) {
   nonce := 0
   for nonce < math.MaxInt64 {
     data := pow.InitData(nonce)
-    hash := sha256.Sum256(data)
+    hash  = sha256.Sum256(data)
 
-    // fmt.Printf("\r%x", hash)
+    fmt.Printf("HASHING ------  \r%x", hash)
     intHash.SetBytes(hash[:])
 
     if intHash.Cmp(pow.Target) == -1 {
@@ -65,7 +65,7 @@ func (pow *ProofOfWork) Run() (int, []byte) {
       nonce++
     }
   }
-  fmt.Println()
+
   return nonce, hash[:]
 }
 
